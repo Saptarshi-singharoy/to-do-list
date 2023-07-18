e6f50d6c53b4555d98729653fdf8371c47065f0a
saptarshi singharoy <you@example.com]>
2023-07-18 16:13:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+
+app= express();
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+items=[];
+
+app.get('/', function(req, res) {
+
+    var today = new Date;
+
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+
+    var day = today.toLocaleDateString('en-US', options)
+
+res.render('list', {kindOfDay: day, newItem: items})}
+
+)
+
+app.post('/', function(req, res){
+    var item = req.body.input
+    items.push(item)
+    res.redirect('/')
+    
+})
+
+
+app.listen(3000, function(){
+    console.log('server running on port 3000');
+})
